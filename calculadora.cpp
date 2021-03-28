a23b90222ba571bf3205637a81a362a8ecc7f371
Juslan Vargas <juslanvargas@gmail.com>
2021-03-28 01:54:40 -0400

    refactoring

--- calculadora.cpp
+++ calculadora.cpp
@@ -2,63 +2,64 @@
 #include <stdio.h>
 using namespace std;
 
+void mostrarMenu(){
+  cout << "\n   MENU CALCULADORA" << endl;
+  cout << "     1. Sumar" << endl;
+  cout << "     2. Restar"<< endl;
+  cout << "     3. Multiplicar"<< endl;
+  cout << "     4. Dividir" << endl;
+  cout << "     5. Salir\n" << endl;
+}
+char elegirOpcion(){
+  char option;
+  do {
+          cout << "\n   Eliga la opcion de su preferencia: ";
+          cin >> option;
+  } while ( option < '1' || option > '5' );
+  return option;
+}
+
+void ingresarNumeros(int *numero1, int *numero2){
+  cout << "\n   Introduzca el 1er num: " ;
+  cin >> *numero1;
+  cout << "\n   Introduzca el 2do num: " ;
+  cin >> *numero2;
+}
+void mostrarRespuesta(int numero1, int numero2, int respuesta,char operacion){
+  cout <<"\n   " << numero1 << operacion << numero2 <<" = "<< respuesta;
+}
+void mostrarError(){
+  cout <<"\n ERROR \n";
+}
 int main(){
     char option;
-    int n1, n2, respuesta;
+    int numero1, numero2, respuesta;
     do {
-        cout << "\n   MENU CALCULADORA" << endl;
-        cout << "     1. Sumar" << endl;
-        cout << "     2. Restar"<< endl;
-        cout << "     3. Multiplicar"<< endl;
-        cout << "     4. Dividir" << endl;
-        cout << "     5. Salir\n" << endl;
+      mostrarMenu();
+      option=elegirOpcion();
+      switch ( option ) {
+          case '1':
+            ingresarNumeros(&numero1,&numero2);
+            respuesta = numero1 + numero2;
+            mostrarRespuesta(numero1,numero2,respuesta,'+');
+            break;
 
-        do {
-            cout << "\n   Eliga la opcion de su preferencia: ";
-            cin >> option;
-        } while ( option < '1' || option > '5' );
+          case '2':
+            ingresarNumeros(&numero1,&numero2);
+            respuesta = numero1 - numero2;
+            mostrarRespuesta(numero1,numero2,respuesta,'-');
+            break;
 
-        switch ( option ) {
-            case '1': 
-              cout << "\n   Introduzca el 1er num: " ;
-              cin >> n1;
-              cout << "\n   Introduzca el 2do num: " ;
-              cin >> n2;
-              respuesta = n1 + n2;
-              cout <<"\n   " << n1 << " + " << n2 <<" = "<< respuesta;
-              break;
+          case '3':
+            ingresarNumeros(&numero1,&numero2);
+            respuesta = numero1 * numero2;
+            mostrarRespuesta(numero1,numero2,respuesta,'*');
+            break;
 
-            case '2': 
-              cout << "\n   Introduzca el 1er num: " ;
-              cin >> n1;
-              cout << "\n   Introduzca el 2do num: " ;
-              cin >> n2;
-              respuesta = n1 - n2;
-              cout <<"\n   " << n1 << " - " << n2 <<" = "<< respuesta;
-              break;
-
-            case '3': 
-              cout << "\n   Introduzca el 1er num: " ;
-              cin >> n1;
-              cout << "\n   Introduzca el 2do num: " ;
-              cin >> n2;
-              respuesta = n1 * n2;
-              cout <<"\n   " << n1 << " * " << n2 <<" = "<< respuesta;
-              break;           
-
-            case '4': 
-              cout << "\n   Introduzca el 1er num: " ;
-              cin >> n1;
-              cout << "\n   Introduzca el 2do num: " ;
-              cin >> n2;
-              if ( n2 != 0 ){
-                respuesta = n1 / n2;
-                cout <<"\n   " << n1 << " / " << n2 <<" = "<< respuesta;
-              }
-              else {
-                cout << "\n   ERROR \n" ;
-              }
-        }
+          case '4':
+            ingresarNumeros(&numero1,&numero2);
+            numero2 != 0 ? mostrarRespuesta(numero1,numero2,respuesta=numero1/numero2,'/') : mostrarError();
+      }
     } while ( option != '5' );
     return 0;
 }
---
+++ refactoring.md
@@ -0,0 +1,44 @@
+**Code Smell 1**
+5-64.
+
+Metodo grande
+
+Creacion de  funciones que hagan tareas unicas y no todo a la vez (dividir y conquistar).
+
+5-33.
+
+**Code Smell 2**
+23-26 ; 32-35 ; 41-44 ; 50-53
+
+Codigo Duplicado.
+
+Extraccion de un metodo.
+
+22-27.
+
+**Code Smell 3**
+28;37;46;56
+
+Codigo Duplicado.
+
+Extraccion de un metodo.
+
+28-30.
+
+**Code Smell 4**
+7.
+
+Identificadores excesivamente cortos, la variable n1 & n2  no daba a enteder claramente que son los numeros que nos pasaran para la operacion.
+
+Cambio de nombre de la variable.
+
+36
+
+**Code Smell 5**
+54-60.
+
+Ternary Elegance.
+
+aplicacion del ternary operator, una manera mas elegenate y entendible del if-else.
+
+61.
